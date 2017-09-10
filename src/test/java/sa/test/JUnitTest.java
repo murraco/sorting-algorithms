af35f7e3a3bc90ab713e120858afed0a564b899b
murraco <mauriurraco@gmail.com>
2017-09-10 18:42:42 -0300

    Minor fix in JUnit testing

--- src/test/java/sa/test/JUnitTest.java
+++ src/test/java/sa/test/JUnitTest.java
@@ -19,7 +19,6 @@ public class JUnitTest {
 	public void testBubbleSort() {
 		final Integer[] data = { 4, 3, 0, 11, 7, 5, 15, 12, 99, 1 };
 		BubbleSort.bubbleSort(data);
-		System.out.println(Arrays.toString(data));
 		assertEquals("[0, 1, 3, 4, 5, 7, 11, 12, 15, 99]", Arrays.toString(data));
 	}
 
