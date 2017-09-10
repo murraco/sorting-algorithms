79fa34af8323f16fe2f4e5fb8590bcd831dbb4f4
murraco <mauriurraco@gmail.com>
2017-09-10 15:41:48 -0300

    Add JUnit testing

---
+++ src/test/java/sa/test/JUnitTest.java
@@ -0,0 +1,60 @@
+package sa.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import sa.sorting.BubbleSort;
+import sa.sorting.Heapsort;
+import sa.sorting.InsertionSort;
+import sa.sorting.MergeSort;
+import sa.sorting.Quicksort;
+import sa.sorting.SelectionSort;
+
+public class JUnitTest {
+
+	@Test
+	public void testBubbleSort() {
+		final Integer[] data = { 4, 3, 0, 11, 7, 5, 15, 12, 99, 1 };
+		BubbleSort.bubbleSort(data);
+		System.out.println(Arrays.toString(data));
+		assertEquals("[0, 1, 3, 4, 5, 7, 11, 12, 15, 99]", Arrays.toString(data));
+	}
+
+	@Test
+	public void testInsertionSort() {
+		final Integer[] data = { 4, 3, 0, 11, 7, 5, 15, 12, 99, 1 };
+		InsertionSort.insertionSort(data);
+		assertEquals("[0, 1, 3, 4, 5, 7, 11, 12, 15, 99]", Arrays.toString(data));
+	}
+
+	@Test
+	public void testSelectionSort() {
+		final Integer[] data = { 4, 3, 0, 11, 7, 5, 15, 12, 99, 1 };
+		SelectionSort.selectionSort(data);
+		assertEquals("[0, 1, 3, 4, 5, 7, 11, 12, 15, 99]", Arrays.toString(data));
+	}
+
+	@Test
+	public void testMergeSort() {
+		final Integer[] data = { 4, 3, 0, 11, 7, 5, 15, 12, 99, 1 };
+		MergeSort.mergeSort(data);
+		assertEquals("[0, 1, 3, 4, 5, 7, 11, 12, 15, 99]", Arrays.toString(data));
+	}
+
+	@Test
+	public void testHeapsort() {
+		final Integer[] data = { 4, 3, 0, 11, 7, 5, 15, 12, 99, 1 };
+		Heapsort.heapSort(data);
+		assertEquals("[0, 1, 3, 4, 5, 7, 11, 12, 15, 99]", Arrays.toString(data));
+	}
+
+	@Test
+	public void testQuicksort() {
+		final Integer[] data = { 4, 3, 0, 11, 7, 5, 15, 12, 99, 1 };
+		Quicksort.quickSort(data);
+		assertEquals("[0, 1, 3, 4, 5, 7, 11, 12, 15, 99]", Arrays.toString(data));
+	}
+}
